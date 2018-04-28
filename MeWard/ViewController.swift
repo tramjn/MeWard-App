f744b5e7ec4e451f7a38d3a1824fd7d20a6afd0c
Tram Nguyen <tramnguyen@trams-mbp.bc.edu>
2018-04-28 01:08:57 -0400

    Initial Commit

---
+++ MeWard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeWard
+//
+//  Created by Tram Nguyen on 4/28/18.
+//  Copyright © 2018 Tram Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
